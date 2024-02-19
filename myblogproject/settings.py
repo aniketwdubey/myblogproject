74d1a41bfbf835bec7b2e0c922e7995626ca772a
Aniket Dubey <aniketdubey@Anikets-MacBook-Air.local>
2024-02-20 00:20:28 +0530

    modified requirements.txt

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "blog",
     'strawberry.django',
+    "blog",
 ]
 
 MIDDLEWARE = [
